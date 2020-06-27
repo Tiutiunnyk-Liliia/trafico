f916dd6560d5c4064f0f879a3bad232e2395e8b9
Tiutiunnyk-Liliia <yalili4ka1210@gmail.com>
2020-06-27 13:51:17 +0300

    Developing section "about" is done

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,14 @@ $(function () {
     $('.faq__item').on('click', function () {
         $(this).toggleClass('active')
     });
+
+    $('.about__slider').slick({
+        infinite: true,
+        //centerMode: true,
+        variableWidth: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/prev.png" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/next.png" alt="">',
+    });
 });
