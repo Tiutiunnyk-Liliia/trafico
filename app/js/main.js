5bda5040961fdf38b02e6a22c4babd5253a90ed2
Tiutiunnyk-Liliia <yalili4ka1210@gmail.com>
2020-07-17 19:14:36 +0300

    Developing section "about" media version is done

--- app/js/main.js
+++ app/js/main.js
@@ -10,12 +10,21 @@ $(function () {
 
     $('.about__slider').slick({
         infinite: true,
-        //centerMode: true,
         variableWidth: true,
         slidesToShow: 2,
         slidesToScroll: 1,
         prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/prev.png" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/next.png" alt="">',
+        responsive:[
+            {
+            breakpoint: 501,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
+                }
+
+        },
+        ]
     });
 
     $('.header__menu-btn').on('click', function () {
